290d8ec54f73c6aaa14e056206da2fae4da9844f
Michael Coughlin <coughlin@physics.harvard.edu>
2019-04-19 16:53:08 -0400

    Add area to call

--- growth/too/models.py
+++ growth/too/models.py
@@ -202,6 +202,7 @@ def create_all():
                                        reference_filter_ids=ref_filter_ids,
                                        ipix=ipix.tolist()))
 
+<<<<<<< HEAD
             if tele == "ZTF":
                 quadrant_coords = get_ztf_quadrants()
 
@@ -715,12 +716,30 @@ class Plan(db.Model):
 
     @property
     def ipix(self):
+<<<<<<< HEAD
         return {i for _ in self.planned_observations for i in _.field.ipix}
 
     @property
     def area(self):
         nside = Localization.nside
         return hp.nside2pixarea(nside, degrees=True) * len(self.ipix)
+=======
+        ipix = {i for _ in self.planned_observations for i in _.field.ipix}
+        return list(ipix)
+
+    @property
+    def area(self):
+
+        nside = Localization.nside
+        pixarea = hp.nside2pixarea(nside)
+        pixarea_deg2 = hp.nside2pixarea(nside, degrees=True)
+
+        if len(self.ipix) == 0:
+            cum_area = 0.0
+        else:
+            cum_area = len(self.ipix) * pixarea_deg2
+
+        return cum_area
 
 
 class PlannedObservation(db.Model):
