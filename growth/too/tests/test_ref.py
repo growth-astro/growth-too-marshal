8527958e1082a2367317cc9bfdb849c1230b6b3f
Michael Coughlin <coughlin@physics.harvard.edu>
2019-04-22 15:51:46 -0400

    Fix Lint

--- growth/too/tests/test_ref.py
+++ growth/too/tests/test_ref.py
@@ -3,11 +3,12 @@ import astropy
 from .. import models
 from ..tasks import ref
 
+
 def test_refs():
     data_rows = [(1, 2, 1, 1, 22.0),
                  (1, 2, 1, 2, 23.0),
                  (2, 2, 1, 2, 23.0)]
-    names=('field', 'ccid', 'qid', 'fid', 'maglimit')
+    names = ('field', 'ccid', 'qid', 'fid', 'maglimit')
     refstable = astropy.table.Table(rows=data_rows, names=names)
     ref.ztf_references(refstable=refstable)
     field = models.Field.query.filter_by(telescope='ZTF', field_id=1).one()
