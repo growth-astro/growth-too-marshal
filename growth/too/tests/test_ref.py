a348954c6a0c803a68bf2c8d76786321964cee20
Michael Coughlin <coughlin@physics.harvard.edu>
2019-04-23 09:29:39 -0400

    Add table file

--- growth/too/tests/test_ref.py
+++ growth/too/tests/test_ref.py
@@ -1,16 +1,17 @@
-import astropy
+import os
+from astropy.io import ascii
 
 from .. import models
 from ..tasks import ref
 
 
 def test_refs():
-    data_rows = [(1, 2, 1, 1, 22.0),
-                 (1, 2, 1, 2, 23.0),
-                 (2, 2, 1, 2, 23.0)]
-    names = ('field', 'ccid', 'qid', 'fid', 'maglimit')
-    refstable = astropy.table.Table(rows=data_rows, names=names)
+
+    payload = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+        'data/ztf_ref_table.dat')
+
+    refstable = ascii.read(payload)
     ref.ztf_references(refstable=refstable)
-    field = models.Field.query.filter_by(telescope='ZTF', field_id=1).one()
-    assert field.reference_filter_ids == [1, 2]
-    assert field.reference_filter_mags == [22.0, 23.0]
+    field = models.Field.query.filter_by(telescope='ZTF', field_id=324).one()
+    assert field.reference_filter_ids == [1]
+    assert field.reference_filter_mags == [21.326900]
