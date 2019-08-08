f7473efd05363fc340964c89110767cce17a4cb9
Michael Coughlin <mcoughli@caltech.edu>
2019-08-08 16:17:25 -0700

    Search for large data files in Flask instance directory (#57)

--- growth/too/models.py
+++ growth/too/models.py
@@ -145,8 +145,11 @@ def create_all():
             reference_images = {}
             reference_mags = {}
 
-        tessfile = pkg_resources.resource_stream(__name__,
-                                                 'input/%s.tess' % tele)
+        tesspath = 'input/%s.tess' % tele
+        try:
+            tessfile = app.open_instance_resource(tesspath)
+        except IOError:
+            tessfile = pkg_resources.resource_stream(__name__, tesspath)
         configfile = pkg_resources.resource_stream(__name__,
                                                    'config/%s.config' % tele)
         with tessfile as f, configfile as g:
