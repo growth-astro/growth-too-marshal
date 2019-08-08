Fix sign of UNIQ pixel indices

growth/too/models.py
@@ -635,8 +635,7 @@ class Localization(db.Model):
     def table_2d(self):
         """Get multiresolution HEALPix dataset, probability density only."""
         return table.Table(
-            [np.asarray(self.uniq, dtype=np.uint64), self.probdensity],
-            names=['UNIQ', 'PROBDENSITY'])
+            [self.uniq, self.probdensity], names=['UNIQ', 'PROBDENSITY'])
 
     @property
     def table(self):
@@ -645,7 +644,7 @@ class Localization(db.Model):
         if self.is_3d:
             return table.Table(
                 [
-                    np.asarray(self.uniq, dtype=np.uint64),
+                    self.uniq,
                     self.probdensity, self.distmu,
                     self.distsigma, self.distnorm],
                 names=[

growth/too/tasks/skymaps.py
@@ -58,7 +58,7 @@ def from_cone(ra, dec, error, dateobs):
     ipix = hpx.cone_search_skycoord(center, 4 * radius)
 
     # Convert to multi-resolution pixel indices and sort.
-    uniq = moc.nest2uniq(nside_to_level(hpx.nside), ipix.astype(np.uint64))
+    uniq = moc.nest2uniq(nside_to_level(hpx.nside), ipix)
     i = np.argsort(uniq)
     ipix = ipix[i]
     uniq = uniq[i]