5df77fe80c0dc19ca2e151945e8b6b9e048cb387
Leo Singer <leo.singer@ligo.org>
2019-08-08 14:32:59 -0700

    Remove Redis selector workaround

--- growth/too/__init__.py
+++ growth/too/__init__.py
@@ -1,13 +1,4 @@
-import redis.selector
-
 from ._version import get_versions
 
 __version__ = get_versions()['version']
 del get_versions
-
-# Set up Redis to use poll() instead of select() because Python's
-# poll() function is not thread safe and therefore does not calling.get()
-# on multiple Celery results concurrently from different threads.
-#
-# See https://bugs.python.org/issue8865
-redis.selector._DEFAULT_SELECTOR = redis.selector.SelectSelector
