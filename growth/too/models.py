d521ed8957bc75026e6a2b8ebd741a68cb09b514
Leo Singer <leo.singer@ligo.org>
2019-08-20 23:37:29 -0400

    Revert "Eliminate bottleneck in populating ZTF subfields"
    This reverts commit 96595b8b877a3fe3193f6b266678778d874a7672.

--- growth/too/models.py
+++ growth/too/models.py
@@ -242,14 +242,12 @@ def create_all():
                 for field_id, xyz in zip(
                         tqdm(fields['field_id'], 'populating subfields'),
                         quadrant_xyz):
-                    db.session.bulk_save_objects([
-                        SubField(telescope=tele,
-                                 field_id=int(field_id),
-                                 subfield_id=int(ii),
-                                 ipix=hp.query_polygon(
-                                     Localization.nside, xyz).tolist())
-                        for ii, xyz in enumerate(xyz)
-                    ])
+                    for ii, xyz in enumerate(xyz):
+                        ipix = hp.query_polygon(Localization.nside, xyz)
+                        db.session.merge(SubField(telescope=tele,
+                                                  field_id=int(field_id),
+                                                  subfield_id=int(ii),
+                                                  ipix=ipix.tolist()))
 
 
 class User(db.Model, UserMixin):
