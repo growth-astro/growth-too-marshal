cf674733e67aca9c26f0f61fd87cd19f88616046
igorandreoni <igor@Igors-MacBook-Pro.local>
2019-08-28 12:14:47 -0700

    Fixed review comments on growthdb_cgi.py

--- growth/too/tasks/growthdb_cgi.py
+++ growth/too/tasks/growthdb_cgi.py
@@ -1,8 +1,11 @@
 import requests
 import json
+import datetime
+import logging
+
 import numpy as np
 import astropy.units as u
-import datetime
+from astropy.time import Time
 from celery.task import PeriodicTask
 from celery.utils.log import get_task_logger
 from celery.local import PromiseProxy
@@ -24,77 +27,73 @@ def get_programidx(program_name):
 
     r = requests.post('http://skipper.caltech.edu:8080/cgi-bin/growth/list_programs.cgi')
     r.raise_for_status()
-    programs = json.loads(r.text)
-    program_dict = {p['name']:p['programidx'] for i,p in enumerate(programs)}
+    programs = r.json()
+    program_dict = {p['name']:p['programidx'] for p in enumerate(programs)}
 
     try:
         return program_dict[program_name]
     except KeyError:
-        print(f'The user {username} does not have access to the program {program_name}')
-        return None
+        logging.error(f"The user does not have access to the GROWTH Marshal program '{program_name}'"))
+        return
 
 
 def get_source_autoannotations(sourceid):
-    """ Fetch a specific source's autoannotations from the GROWTH marshal and 
-    create a string with the autoannotations available. """
+    """Fetch a specific source's autoannotations from the GROWTH marshal and 
+    create a string with the autoannotations available."""
     r = requests.post('http://skipper.caltech.edu:8080/cgi-bin/growth/source_summary.cgi', data={'sourceid':str(sourceid)
 }) 
     r.raise_for_status()
-    summary=json.loads(r.text)
-    autoannotations=summary['autoannotations']
-    autoannotations_string = ""
-    for auto in autoannotations:
-        autoannotations_string = f"{autoannotations_string}; \n {auto['username']}, {auto['type']}, {auto['comment']}" 
+    summary = r.json()
+    autoannotations = summary['autoannotations']
+    autoannotations_string = '; '.join(f"{auto['username']}, {auto['type']}, {auto['comment']}" for auto in autoannotations)
 
     return autoannotations_string
 
 
 def get_candidates_growth_marshal(program_name):
-    """ Query the GROWTH db for the science programs """
+    """Query the GROWTH db for the science programs"""
 
-    programidx=get_programidx(program_name)
-    if programidx==None:
-        return None
+    programidx = get_programidx(program_name)
+    if programidx is None:
+        return
     r = requests.post('http://skipper.caltech.edu:8080/cgi-bin/growth/list_program_sources.cgi', data={'programidx':str(programidx)})
     r.raise_for_status()
-    sources=json.loads(r.text)
-    """ Add autoannotations """
-    for s, i in zip(sources, np.arange(len(sources))):
-        autoannotations=get_source_autoannotations(s["id"])
-        sources[i]['autoannotations'] = autoannotations
-
-    return sources
+    sources = r.json()
+    #Add autoannotations
+    for source in r.json():
+        yield dict(source, annotations=get_source_autoannotations(source["id"]))
 
 
 def update_local_db_growthmarshal(sources, program_name):
-    """ Takes the candidates fetched from the GROWTH marshal and 
-    updates the local database using SQLAlchemy. """
+    """Takes the candidates fetched from the GROWTH marshal and 
+    updates the local database using SQLAlchemy."""
+
     for s in sources:
-        print("printing s:", s)
-        creationdate_list_int=[int(d) for d in s['creationdate'].split('-')]
-        lastmodified_list_int=[int(d) for d in s['lastmodified'].split('-')]
-        #autoannotations_string = get_source_autoannotations(sourceid, username, password)
+        creationdate = Time(s['creationdate'], fromat='iso').datetime
+        lastmodified = Time(s['lastmodified'], fromat='iso').datetime
 
         try:
             rcid = int(s['rcid'])
-            field = int(s['field'])
-            candid = int(s['candid'])
         except (KeyError, ValueError):
             rcid = None
+        try:
+            field = int(s['field'])
+        except (KeyError, ValueError):
             field = None
+        try:
+            candid = int(s['candid'])
+        except (KeyError, ValueError):
             candid = None
-
         try:
             redshift = float(s['redshift'])
-        except:
+        except (KeyError, ValueError):
             redshift = None
 
         models.db.session.merge( 
-            models.Candidates(
+            models.Candidate(
                 name=s['name'],
                 subfield_id=rcid,
-                creationdate=datetime.datetime(creationdate_list_int[0],\
-                creationdate_list_int[1],creationdate_list_int[2]),
+                creationdate=creationdate,
                 classification=s['classification'],
                 redshift=redshift,
                 iauname=s['iauname'],
@@ -102,8 +101,7 @@ def update_local_db_growthmarshal(sources, program_name):
                 candid=candid,
                 ra=float(s['ra']),
                 dec=float(s['dec']),
-                lastmodified=datetime.datetime(lastmodified_list_int[0],\
-                lastmodified_list_int[1],lastmodified_list_int[2]),
+                lastmodified=lastmodified,
                 autoannotations=s['autoannotations']
                 )
         )
@@ -113,13 +111,11 @@ def update_local_db_growthmarshal(sources, program_name):
 @celery.task(base=PeriodicTask, shared=False, run_every=datetime.timedelta(seconds=180))
 def fetch_candidates_growthmarshal():
     """Fetch the candidates present in the GROWTH marshal
-    for the MMA science programs and store them in the local db. """ 
+    for the MMA science programs and store them in the local db.""" 
 
     program_names = ['DECAM GW Followup', 'Afterglows of Fermi Gamma Ray Bursts', \
     'Electromagnetic Counterparts to Neutrinos', 'Electromagnetic Counterparts to Gravitational Waves']
 
     for program_name in program_names:
-        sources=get_candidates_growth_marshal(program_name)
+        sources = get_candidates_growth_marshal(program_name)
         update_local_db_growthmarshal(sources, program_name)
-
-    return
