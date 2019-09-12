6fc3817e479878d2f3ea4564ab2a2544585428d3
igorandreoni <igor.andreoni@gmail.com>
2019-09-12 10:44:01 -0700

    fixed lint

--- growth/too/tasks/growthdb_cgi.py
+++ growth/too/tasks/growthdb_cgi.py
@@ -11,10 +11,10 @@ from .. import models
 """
 Reminder for the relevant program names:
 decam_programidx=program_dict['DECAM GW Followup']
-em_gw_programidx=program_dict['Electromagnetic Counterparts 
+em_gw_programidx=program_dict['Electromagnetic Counterparts
 to Gravitational Waves']
 fermi_programidx=program_dict['Afterglows of Fermi Gamma Ray Bursts']
-neutrino_programidx=program_dict['Electromagnetic Counterparts 
+neutrino_programidx=program_dict['Electromagnetic Counterparts
 to Neutrinos']
 """
 
@@ -36,16 +36,16 @@ def get_programidx(program_name):
 
 
 def get_source_autoannotations(sourceid):
-    """Fetch a specific source's autoannotations from the GROWTH marshal and 
-create a string with the autoannotations available."""
+    """Fetch a specific source's autoannotations from the GROWTH marshal and
+    create a string with the autoannotations available."""
     r = requests.post(
-        'http://skipper.caltech.edu:8080/cgi-bin/growth/source_summary.cgi', 
-        data={'sourceid': str(sourceid)}) 
+        'http://skipper.caltech.edu:8080/cgi-bin/growth/source_summary.cgi',
+        data={'sourceid': str(sourceid)})
     r.raise_for_status()
     summary = r.json()
     autoannotations = summary['autoannotations']
     autoannotations_string = '; '.join(
-        f"{auto['username']}, {auto['type']}, {auto['comment']}" 
+        f"{auto['username']}, {auto['type']}, {auto['comment']}"
         for auto in autoannotations)
 
     return autoannotations_string
@@ -58,19 +58,20 @@ def get_candidates_growth_marshal(program_name):
     if programidx is None:
         return
     r = requests.post(
-        'http://skipper.caltech.edu:8080/cgi-bin/growth/list_program_sources.cgi', 
+        'http://skipper.caltech.edu:8080/cgi-bin/growth/list\
+        _program_sources.cgi',
         data={'programidx': str(programidx)})
     r.raise_for_status()
     sources = r.json()
     # Add autoannotations
     for source in sources:
-        yield dict(source, 
-            annotations=get_source_autoannotations(source["id"]))
+        yield dict(source,
+        annotations=get_source_autoannotations(source["id"]))
 
 
 def update_local_db_growthmarshal(sources, program_name):
-    """Takes the candidates fetched from the GROWTH marshal and 
-updates the local database using SQLAlchemy."""
+    """Takes the candidates fetched from the GROWTH marshal and
+    updates the local database using SQLAlchemy."""
 
     for s in sources:
         creationdate = Time(s['creationdate'], fromat='iso').datetime
@@ -112,16 +113,16 @@ updates the local database using SQLAlchemy."""
     models.db.session.commit()
 
 
-@celery.task(base=PeriodicTask, 
-    shared=False, run_every=datetime.timedelta(seconds=180))
+@celery.task(base=PeriodicTask,
+shared=False, run_every=datetime.timedelta(seconds=180))
 def fetch_candidates_growthmarshal():
     """Fetch the candidates present in the GROWTH marshal
-    for the MMA science programs and store them in the local db.""" 
+    for the MMA science programs and store them in the local db."""
 
-    program_names = ['DECAM GW Followup', 
-        'Afterglows of Fermi Gamma Ray Bursts',
-        'Electromagnetic Counterparts to Neutrinos', 
-        'Electromagnetic Counterparts to Gravitational Waves']
+    program_names = ['DECAM GW Followup',
+    'Afterglows of Fermi Gamma Ray Bursts',
+    'Electromagnetic Counterparts to Neutrinos',
+    'Electromagnetic Counterparts to Gravitational Waves']
 
     for program_name in program_names:
         sources = get_candidates_growth_marshal(program_name)
