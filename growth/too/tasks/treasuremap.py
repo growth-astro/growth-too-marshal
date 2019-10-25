ec8e8ddb7772ccc8a866f2b559694da8e1d55ffc
Michael Coughlin <coughlin@physics.harvard.edu>
2019-10-25 12:08:20 -0700

    Include new instrument ids

--- growth/too/tasks/treasuremap.py
+++ growth/too/tasks/treasuremap.py
@@ -13,6 +13,18 @@ bands = {1: 'g', 2: 'r', 3: 'i', 4: 'z', 5: 'J'}
 
 @celery.task(shared=False)
 def observations(dateobs, telescope, observations):
+
+    if telescope == "ZTF":
+        telescope_id = 47
+    elif telescope == "Gattini":
+        telescope_id = 44
+    elif telescope == "DECam":
+        telescope_id = 38
+    elif telescope == "KPED":
+        telescope_id = 45
+    elif telescope == "GROWTH-India":
+        telescope_id = 46
+
     event = models.Event.query.get_or_404(dateobs)
 
     data = {"graceid": event.graceid,
@@ -23,17 +35,18 @@ def observations(dateobs, telescope, observations):
     for ii, observation in enumerate(observations):
         if observation.observation_id in observation_ids:
             continue
-       
+
+        obstime = time.Time(observation.obstime, format="datetime").isot
+
         pointing = {}
         pointing["ra"] = observation.field.ra
         pointing["dec"] = observation.field.dec
         pointing["band"] = bands[observation.filter_id][0]
-        pointing["instrumentid"] = telescope
+        pointing["instrumentid"] = str(telescope_id)
         pointing["depth"] = observation.limmag
         pointing["depth_unit"] = "ab_mag"
         pointing["status"] = "completed"
-        pointing["time"] = time.Time(observation.obstime,
-                                     format="datetime").isot
+        pointing["time"] = obstime.isot.split(".")[0]
         pointings.append(pointing)
         observation_ids.append(observation.observation_id)
 
@@ -50,14 +63,19 @@ def plan(dateobs, telescope, plan_name):
 
     if telescope == "ZTF":
         depth = 20.5
+        telescope_id = 47
     elif telescope == "Gattini":
         depth = 16.5
+        telescope_id = 44
     elif telescope == "DECam":
         depth = 22.0
+        telescope_id = 38
     elif telescope == "KPED":
         depth = 21.0
+        telescope_id = 45
     elif telescope == "GROWTH-India":
         depth = 20.0
+        telescope_id = 46
 
     tstart = time.Time(json_data['validity_window_mjd'][0], format='mjd')
     event = models.Event.query.get_or_404(dateobs)
@@ -71,11 +89,11 @@ def plan(dateobs, telescope, plan_name):
         pointing["ra"] = data_row["ra"]
         pointing["dec"] = data_row["dec"]
         pointing["band"] = bands[data_row["filter_id"]][0]
-        pointing["instrumentid"] = telescope
+        pointing["instrumentid"] = str(telescope_id)
         pointing["depth"] = str(depth)
         pointing["depth_unit"] = "ab_mag"
         pointing["status"] = "planned"
-        pointing["time"] = tstart.isot
+        pointing["time"] = tstart.isot.split(".")[0]
         pointings.append(pointing)
 
     data["pointings"] = pointings
